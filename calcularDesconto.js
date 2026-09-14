faf79f63e2850d9e943f205c61b35fcff2138104
JadsonC <jcss@ic.ufal.br>
2026-09-14 15:38:47 -0300

    ci: adicionando testes, padronização e ci no projeto

--- calcularDesconto.js
+++ calcularDesconto.js
@@ -1,7 +1,7 @@
 export function calcularDesconto(valor, percentual) {
-    if (percentual < 0 || percentual > 100) {
-        throw new Error('Percentual inválido');
-    }
+  if (percentual < 0 || percentual > 100) {
+    throw new Error('Percentual inválido');
+  }
 
-    return valor * (percentual / 100);
+  return valor * (percentual / 100);
 }
